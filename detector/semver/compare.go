123e998fa6a44bf92dfc3999a6f4ace0a235115c
Gareth Jones <jones258@gmail.com>
2022-03-04 08:20:33 +1300

    feat: switch OSV over to using internal semver package for comparing versions

--- detector/semver/compare.go
+++ detector/semver/compare.go
@@ -85,3 +85,18 @@ func (v *Version) Compare(w Version) int {
 
 	return compareBuilds(v.Build, w.Build)
 }
+
+// CompareStr returns an integer comparing two versions according to
+// semantic version precedence, then by their build version (if present)
+// The result will be 0 if v == w, -1 if v < w, or +1 if v > w.
+//
+// Versions with a build are considered less than ones without (if both
+// have equal components)
+//
+// Builds are compared using "best effort" - generally if a build ends with
+// a number, that will be used as the main comparator.
+func (v *Version) CompareStr(str string) int {
+	w := Parse(str)
+
+	return v.Compare(w)
+}
