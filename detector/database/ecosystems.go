068ce0a98fc5de644cb20edcf85b97c1f968ebf5
Gareth Jones <Jones258@Gmail.com>
2022-03-10 16:54:40 +1300

    fix: sort ecosystems by name when listing (#49)

--- detector/database/ecosystems.go
+++ detector/database/ecosystems.go
@@ -1,6 +1,9 @@
 package database
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func toSliceOfEcosystems(ecosystemsMap map[Ecosystem]struct{}) []Ecosystem {
 	ecosystems := make([]Ecosystem, 0, len(ecosystemsMap))
@@ -27,7 +30,13 @@ func (db *OSVDatabase) ListEcosystems() []Ecosystem {
 		}
 	}
 
-	return toSliceOfEcosystems(ecosystems)
+	slicedEcosystems := toSliceOfEcosystems(ecosystems)
+
+	sort.Slice(slicedEcosystems, func(i, j int) bool {
+		return slicedEcosystems[i] < slicedEcosystems[j]
+	})
+
+	return slicedEcosystems
 }
 
 func (db *OSVDatabase) ListEcosystemVulnerabilities(ecosystem Ecosystem) []OSV {
