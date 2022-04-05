19d533bf66c4d6c46003cd338a0f23a957517c93
Gareth Jones <jones258@gmail.com>
2022-04-05 17:56:04 +1200

    test: write tests for `Reporter`

---
+++ internal/reporter/reporter_test.go
@@ -0,0 +1,77 @@
+package reporter_test
+
+import (
+	"bytes"
+	"osv-detector/internal/reporter"
+	"testing"
+)
+
+type TestResult struct {
+	String string `json:"value"`
+}
+
+func (r TestResult) ToString() string {
+	return r.String
+}
+
+func TestReporter_PrintExtra(t *testing.T) {
+	msg := "Hello world!"
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	r := reporter.New(stdout, stderr, false)
+
+	r.PrintExtra(msg)
+
+	if gotStdout := stdout.String(); gotStdout != "" {
+		t.Errorf("Expected stdout to be empty, but got \"%s\"", gotStdout)
+	}
+
+	if gotStderr := stderr.String(); gotStderr != msg {
+		t.Errorf("Expected stderr to have \"%s\", but got \"%s\"", msg, gotStderr)
+	}
+}
+
+func TestReporter_PrintResult(t *testing.T) {
+	msg := "Hello world!"
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	r := reporter.New(stdout, stderr, false)
+
+	r.PrintResult(TestResult{String: msg})
+
+	if gotStdout := stdout.String(); gotStdout != msg {
+		t.Errorf("Expected stdout to have \"%s\", but got \"%s\"", msg, gotStdout)
+	}
+
+	if gotStderr := stderr.String(); gotStderr != "" {
+		t.Errorf("Expected stderr to be empty, but got \"%s\"", gotStderr)
+	}
+}
+
+func TestReporter_PrintResult_OutputAsJSON(t *testing.T) {
+	msg := "Hello world!"
+	json := "{\"results\":[{\"value\":\"Hello world!\"}]}"
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	r := reporter.New(stdout, stderr, true)
+
+	r.PrintResult(TestResult{String: msg})
+
+	if gotStdout := stdout.String(); gotStdout != "" {
+		t.Errorf("Expected stdout to be empty, but got \"%s\"", gotStdout)
+	}
+
+	if gotStderr := stderr.String(); gotStderr != "" {
+		t.Errorf("Expected stderr to be empty, but got \"%s\"", gotStderr)
+	}
+
+	r.PrintJSONResults()
+
+	if gotStdout := stdout.String(); gotStdout != json {
+		t.Errorf("Expected stdout to be \"%s\", but got \"%s\"", json, gotStdout)
+	}
+
+	if gotStderr := stderr.String(); gotStderr != "" {
+		t.Errorf("Expected stderr to be empty, but got \"%s\"", gotStderr)
+	}
+}
