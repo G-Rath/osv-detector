f8dd1bf50a03d596ecc0d983df3dcf52ca17fda3
Gareth Jones <jones258@gmail.com>
2022-04-08 09:07:19 +1200

    fix: remove unneeded debug logs

--- internal/database/ecosystems.go
+++ internal/database/ecosystems.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"sort"
 )
 
@@ -19,12 +18,6 @@ func (db *OSVDatabase) ListEcosystems() []Ecosystem {
 	ecosystems := make(map[Ecosystem]struct{})
 
 	for _, vulnerability := range db.Vulnerabilities(true) {
-		if vulnerability.Affected == nil {
-			fmt.Printf("Skipping %s as it does not have an 'affected' property", vulnerability.ID)
-
-			continue
-		}
-
 		for _, affected := range vulnerability.Affected {
 			ecosystems[affected.Package.Ecosystem] = struct{}{}
 		}
@@ -43,12 +36,6 @@ func (db *OSVDatabase) ListEcosystemVulnerabilities(ecosystem Ecosystem) []OSV {
 	var vulnerabilities []OSV
 
 	for _, vulnerability := range db.Vulnerabilities(false) {
-		if vulnerability.Affected == nil {
-			fmt.Printf("Skipping %s as it does not have an 'affected' property", vulnerability.ID)
-
-			continue
-		}
-
 		if vulnerability.AffectsEcosystem(ecosystem) {
 			vulnerabilities = append(vulnerabilities, vulnerability)
 		}
