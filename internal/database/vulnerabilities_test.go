060841a02a0e5c0e113c8dd67c9956777cc84fb7
Gareth Jones <jones258@gmail.com>
2022-05-03 08:27:47 +1200

    test: add some cases for `Vulnerabilities#MarshalJSON`

--- internal/database/vulnerabilities_test.go
+++ internal/database/vulnerabilities_test.go
@@ -1,6 +1,7 @@
 package database_test
 
 import (
+	"encoding/json"
 	"osv-detector/internal/database"
 	"testing"
 )
@@ -121,6 +122,61 @@ func TestVulnerabilities_Includes(t *testing.T) {
 
 			if got := tt.vs.Includes(tt.args.osv); got != tt.want {
 				t.Errorf("Includes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVulnerabilities_MarshalJSON(t *testing.T) {
+	t.Parallel()
+
+	osv := database.OSV{ID: "GHSA-1"}
+	asJSON, err := json.Marshal(osv)
+
+	if err != nil {
+		t.Fatalf("Unable to marshal osv to JSON: %v", err)
+	}
+
+	tests := []struct {
+		name string
+		vs   database.Vulnerabilities
+		want string
+	}{
+		{
+			name: "",
+			vs:   nil,
+			want: "[]",
+		},
+		{
+			name: "",
+			vs:   database.Vulnerabilities(nil),
+			want: "[]",
+		},
+		{
+			name: "",
+			vs:   database.Vulnerabilities{osv},
+			want: "[" + string(asJSON) + "]",
+		},
+		{
+			name: "",
+			vs:   database.Vulnerabilities{osv, osv},
+			want: "[" + string(asJSON) + "," + string(asJSON) + "]",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := tt.vs.MarshalJSON()
+			if err != nil {
+				t.Errorf("MarshalJSON() error = %v", err)
+
+				return
+			}
+
+			if gotStr := string(got); gotStr != tt.want {
+				t.Errorf("MarshalJSON() got = %v, want %v", gotStr, tt.want)
 			}
 		})
 	}
