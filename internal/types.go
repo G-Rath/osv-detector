4143dc04662304b5224d8cd7ee5170d2c7379767
Gareth Jones <jones258@gmail.com>
2022-06-17 08:25:13 +1200

    feat: support passing commits to csv parsers

--- internal/types.go
+++ internal/types.go
@@ -4,7 +4,7 @@ type PackageDetails struct {
 	Name      string    `json:"name"`
 	Version   string    `json:"version"`
 	Commit    string    `json:"commit,omitempty"`
-	Ecosystem Ecosystem `json:"ecosystem"`
+	Ecosystem Ecosystem `json:"ecosystem,omitempty"`
 }
 
 type Ecosystem string
