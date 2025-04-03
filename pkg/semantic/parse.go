c4d5751a2201cf15371f3462aec99b7df9f2e864
Gareth Jones <Jones258@Gmail.com>
2025-04-03 16:35:15 +1300

    feat: add support for comparing RedHat versions (#298)

--- pkg/semantic/parse.go
+++ pkg/semantic/parse.go
@@ -45,6 +45,8 @@ func Parse(str string, ecosystem internal.Ecosystem) (Version, error) {
 		return parseSemverVersion(str), nil
 	case "PyPI":
 		return parsePyPIVersion(str), nil
+	case "Red Hat":
+		return parseRedHatVersion(str), nil
 	case "RubyGems":
 		return parseRubyGemsVersion(str), nil
 	case "Ubuntu":
