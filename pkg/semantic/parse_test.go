3f7e2a886a4335a4fd5ef27bba5ea50646de1b5a
Gareth Jones <jones258@gmail.com>
2025-04-04 07:11:32 +1300

    test(semantic): add `Red Hat` too

--- pkg/semantic/parse_test.go
+++ pkg/semantic/parse_test.go
@@ -13,7 +13,7 @@ func TestParse(t *testing.T) {
 
 	ecosystems := lockfile.KnownEcosystems()
 
-	ecosystems = append(ecosystems, "Alpine", "Debian", "Ubuntu")
+	ecosystems = append(ecosystems, "Alpine", "Debian", "Ubuntu", "Red Hat")
 
 	for _, ecosystem := range ecosystems {
 		_, err := semantic.Parse("", ecosystem)
@@ -35,7 +35,7 @@ func TestMustParse(t *testing.T) {
 
 	ecosystems := lockfile.KnownEcosystems()
 
-	ecosystems = append(ecosystems, "Alpine", "Debian", "Ubuntu")
+	ecosystems = append(ecosystems, "Alpine", "Debian", "Ubuntu", "Red Hat")
 
 	for _, ecosystem := range ecosystems {
 		semantic.MustParse("", ecosystem)
