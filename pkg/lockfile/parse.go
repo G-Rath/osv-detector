fd5296d02d34ac6f1a998a814af982a93f015492
Gareth Jones <jones258@gmail.com>
2025-04-04 07:28:01 +1300

    feat: support parsing `pylock.toml` files

--- pkg/lockfile/parse.go
+++ pkg/lockfile/parse.go
@@ -29,6 +29,7 @@ var parsers = map[string]PackageDetailsParser{
 	"pdm.lock":                    ParsePdmLock,
 	"pnpm-lock.yaml":              ParsePnpmLock,
 	"poetry.lock":                 ParsePoetryLock,
+	"pylock.toml":                 ParsePylock,
 	"Pipfile.lock":                ParsePipenvLock,
 	"pom.xml":                     ParseMavenLock,
 	"pubspec.lock":                ParsePubspecLock,
