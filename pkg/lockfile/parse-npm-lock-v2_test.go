68ca05085759197d3f6a36ad01b7bc1506a67f6f
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-21 14:11:19 +1200

    test: use `errors.Is` to check for `os.ErrNotExist` in lockfile cases

--- pkg/lockfile/parse-npm-lock-v2_test.go
+++ pkg/lockfile/parse-npm-lock-v2_test.go
@@ -1,6 +1,8 @@
 package lockfile_test
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/g-rath/osv-detector/pkg/lockfile"
@@ -11,7 +13,9 @@ func TestParseNpmLock_v2_FileDoesNotExist(t *testing.T) {
 
 	packages, err := lockfile.ParseNpmLock("testdata/npm/does-not-exist")
 
-	expectErrContaining(t, err, "could not read")
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Errorf("expected \"%v\" error but got \"%v\"", os.ErrNotExist, err)
+	}
 	expectPackages(t, packages, []lockfile.PackageDetails{})
 }
 
