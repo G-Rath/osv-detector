210aa84d871c0e74215d9a0cdb0b84de9b034756
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-21 14:28:34 +1200

    feat: replace `package-lock.json` parser

--- go.mod
+++ go.mod
@@ -14,18 +14,33 @@ require (
 )
 
 require (
+	github.com/CycloneDX/cyclonedx-go v0.9.2 // indirect
+	github.com/anchore/go-struct-converter v0.0.0-20230627203149-c72ef8859ca9 // indirect
 	github.com/gkampitakis/ciinfo v0.3.2 // indirect
 	github.com/gkampitakis/go-diff v1.3.2 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.6.2 // indirect
+	github.com/go-git/go-git/v5 v5.16.2 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/maruel/natural v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/ossf/osv-schema/bindings/go v0.0.0-20250805051309-c463400aa925 // indirect
+	github.com/package-url/packageurl-go v0.1.3 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/spdx/tools-golang v0.5.5 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
+	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/protobuf v1.36.8 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- pkg/lockfile/parse-npm-lock.go
+++ pkg/lockfile/parse-npm-lock.go
@@ -1,32 +1,8 @@
 package lockfile
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"path"
-	"strings"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/packagelockjson"
 )
-
-type NpmLockDependency struct {
-	Version      string                       `json:"version"`
-	Dependencies map[string]NpmLockDependency `json:"dependencies,omitempty"`
-}
-
-type NpmLockPackage struct {
-	Name         string            `json:"name"`
-	Version      string            `json:"version"`
-	Resolved     string            `json:"resolved"`
-	Dependencies map[string]string `json:"dependencies"`
-}
-
-type NpmLockfile struct {
-	Version int `json:"lockfileVersion"`
-	// npm v1- lockfiles use "dependencies"
-	Dependencies map[string]NpmLockDependency `json:"dependencies"`
-	// npm v2+ lockfiles use "packages"
-	Packages map[string]NpmLockPackage `json:"packages,omitempty"`
-}
 
 const NpmEcosystem Ecosystem = "npm"
 
@@ -54,121 +30,6 @@ func mergePkgDetailsMap(m1 map[string]PackageDetails, m2 map[string]PackageDetai
 	return details
 }
 
-func parseNpmLockDependencies(dependencies map[string]NpmLockDependency) map[string]PackageDetails {
-	details := map[string]PackageDetails{}
-
-	for name, detail := range dependencies {
-		if detail.Dependencies != nil {
-			details = mergePkgDetailsMap(details, parseNpmLockDependencies(detail.Dependencies))
-		}
-
-		version := detail.Version
-		finalVersion := version
-		commit := ""
-
-		// we can't resolve a version from a "file:" dependency
-		if strings.HasPrefix(detail.Version, "file:") {
-			finalVersion = ""
-		} else {
-			// use the name of the underlying package rather than the alias
-			if strings.HasPrefix(detail.Version, "npm:") {
-				i := strings.LastIndex(detail.Version, "@")
-				name = detail.Version[4:i]
-				finalVersion = detail.Version[i+1:]
-			}
-
-			commit = tryExtractCommit(detail.Version)
-
-			// if there is a commit, we want to deduplicate based on that rather than
-			// the version (the versions must match anyway for the commits to match)
-			//
-			// we also don't actually know what the "version" is, so blank it
-			if commit != "" {
-				finalVersion = ""
-				version = commit
-			}
-		}
-
-		details[name+"@"+version] = PackageDetails{
-			Name:      name,
-			Version:   finalVersion,
-			Ecosystem: NpmEcosystem,
-			CompareAs: NpmEcosystem,
-			Commit:    commit,
-		}
-	}
-
-	return details
-}
-
-func extractNpmPackageName(name string) string {
-	maybeScope := path.Base(path.Dir(name))
-	pkgName := path.Base(name)
-
-	if strings.HasPrefix(maybeScope, "@") {
-		pkgName = maybeScope + "/" + pkgName
-	}
-
-	return pkgName
-}
-
-func parseNpmLockPackages(packages map[string]NpmLockPackage) map[string]PackageDetails {
-	details := map[string]PackageDetails{}
-
-	for namePath, detail := range packages {
-		if namePath == "" {
-			continue
-		}
-
-		finalName := detail.Name
-		if finalName == "" {
-			finalName = extractNpmPackageName(namePath)
-		}
-
-		finalVersion := detail.Version
-
-		commit := tryExtractCommit(detail.Resolved)
-
-		// if there is a commit, we want to deduplicate based on that rather than
-		// the version (the versions must match anyway for the commits to match)
-		if commit != "" {
-			finalVersion = commit
-		}
-
-		details[finalName+"@"+finalVersion] = PackageDetails{
-			Name:      finalName,
-			Version:   detail.Version,
-			Ecosystem: NpmEcosystem,
-			CompareAs: NpmEcosystem,
-			Commit:    commit,
-		}
-	}
-
-	return details
-}
-
-func parseNpmLock(lockfile NpmLockfile) map[string]PackageDetails {
-	if lockfile.Packages != nil {
-		return parseNpmLockPackages(lockfile.Packages)
-	}
-
-	return parseNpmLockDependencies(lockfile.Dependencies)
-}
-
 func ParseNpmLock(pathToLockfile string) ([]PackageDetails, error) {
-	var parsedLockfile *NpmLockfile
-
-	lockfileContents, err := os.ReadFile(pathToLockfile)
-
-	if err != nil {
-		return []PackageDetails{}, fmt.Errorf("could not read %s: %w", pathToLockfile, err)
-	}
-
-	err = json.Unmarshal(lockfileContents, &parsedLockfile)
-
-	if err != nil {
-		return []PackageDetails{}, fmt.Errorf("could not parse %s: %w", pathToLockfile, err)
-	}
-
-	return pkgDetailsMapToSlice(parseNpmLock(*parsedLockfile)), nil
+	return extract(pathToLockfile, packagelockjson.NewDefault(), NpmEcosystem)
 }
