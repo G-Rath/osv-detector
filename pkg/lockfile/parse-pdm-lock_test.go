bf09dcbac4f97104faa13b4210771c8c1c6394b5
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-21 14:11:19 +1200

    test: use `errors.Is` to check for `os.ErrNotExist` in lockfile cases

--- pkg/lockfile/parse-pdm-lock_test.go
+++ pkg/lockfile/parse-pdm-lock_test.go
@@ -1,6 +1,8 @@
 package lockfile_test
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/g-rath/osv-detector/pkg/lockfile"
@@ -18,7 +20,9 @@ func TestParsePdmLock_FileDoesNotExist(t *testing.T) {
 
 	packages, err := lockfile.ParsePdmLock("testdata/pdm/does-not-exist")
 
-	expectErrContaining(t, err, "could not read")
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Errorf("expected \"%v\" error but got \"%v\"", os.ErrNotExist, err)
+	}
 	expectPackages(t, packages, []lockfile.PackageDetails{})
 }
 
