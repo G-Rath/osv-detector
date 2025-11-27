5ea0449f7798e86629908b4bc2437eae5afb5b59
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-11-27 14:13:51 +1300

    feat: update tests and parser

--- pkg/lockfile/parse-pylock.go
+++ pkg/lockfile/parse-pylock.go
@@ -7,12 +7,24 @@ import (
 	"github.com/BurntSushi/toml"
 )
 
+type pylockVCS struct {
+	Type   string `toml:"type"`
+	Commit string `toml:"commit-id"`
+}
+
+type pylockDirectory struct {
+	Path string `toml:"path"`
+}
+
 type PylockPackage struct {
-	Name    string `toml:"name"`
-	Version string `toml:"version"`
+	Name      string          `toml:"name"`
+	Version   string          `toml:"version"`
+	VCS       pylockVCS       `toml:"vcs"`
+	Directory pylockDirectory `toml:"directory"`
 }
 
 type PylockLockfile struct {
+	Version  string          `toml:"lock-version"`
 	Packages []PylockPackage `toml:"packages"`
 }
 
@@ -36,12 +48,23 @@ func ParsePylock(pathToLockfile string) ([]PackageDetails, error) {
 	packages := make([]PackageDetails, 0, len(parsedLockfile.Packages))
 
 	for _, pkg := range parsedLockfile.Packages {
-		packages = append(packages, PackageDetails{
+		// this is likely the root package, which is sometimes included in the lockfile
+		if pkg.Version == "" && pkg.Directory.Path == "." {
+			continue
+		}
+
+		pkgDetails := PackageDetails{
 			Name:      pkg.Name,
 			Version:   pkg.Version,
 			Ecosystem: PylockEcosystem,
 			CompareAs: PylockEcosystem,
-		})
+		}
+
+		if pkg.VCS.Commit != "" {
+			pkgDetails.Commit = pkg.VCS.Commit
+		}
+
+		packages = append(packages, pkgDetails)
 	}
 
 	return packages, nil
--- pkg/lockfile/parse-pylock_test.go
+++ pkg/lockfile/parse-pylock_test.go
@@ -101,3 +101,189 @@ func TestParsePylock_Example(t *testing.T) {
 		},
 	})
 }
+
+func TestParsePylock_PackageWithCommits(t *testing.T) {
+	t.Parallel()
+
+	packages, err := lockfile.ParsePylock("testdata/pylock/commits.toml")
+
+	if err != nil {
+		t.Errorf("Got unexpected error: %v", err)
+	}
+
+	expectPackages(t, packages, []lockfile.PackageDetails{
+		{
+			Name:      "click",
+			Version:   "8.2.1",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "mleroc",
+			Version:   "0.1.0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+			Commit:    "735093f03c4d8be70bfaaae44074ac92d7419b6d",
+		},
+		{
+			Name:      "packaging",
+			Version:   "24.2",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "pathspec",
+			Version:   "0.12.1",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "python-dateutil",
+			Version:   "2.9.0.post0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "scikit-learn",
+			Version:   "1.6.1",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "tqdm",
+			Version:   "4.67.1",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+	})
+}
+
+func TestParsePylock_CreatedByPipWithJustSelf(t *testing.T) {
+	t.Parallel()
+
+	packages, err := lockfile.ParsePylock("testdata/pylock/pip-just-self.toml")
+
+	if err != nil {
+		t.Errorf("Got unexpected error: %v", err)
+	}
+
+	expectPackages(t, packages, []lockfile.PackageDetails{})
+}
+
+func TestParsePylock_CreatedByPip(t *testing.T) {
+	t.Parallel()
+
+	// from https://peps.python.org/pep-0751/#example
+	packages, err := lockfile.ParsePylock("testdata/pylock/pip-full.toml")
+
+	if err != nil {
+		t.Errorf("Got unexpected error: %v", err)
+	}
+
+	expectPackages(t, packages, []lockfile.PackageDetails{
+		{
+			Name:      "annotated-types",
+			Version:   "0.7.0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "packaging",
+			Version:   "25.0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "pyproject-toml",
+			Version:   "0.1.0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "setuptools",
+			Version:   "80.9.0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "wheel",
+			Version:   "0.45.1",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+	})
+}
+
+func TestParsePylock_CreatedByPdm(t *testing.T) {
+	t.Parallel()
+
+	// from https://peps.python.org/pep-0751/#example
+	packages, err := lockfile.ParsePylock("testdata/pylock/pdm-full.toml")
+
+	if err != nil {
+		t.Errorf("Got unexpected error: %v", err)
+	}
+
+	expectPackages(t, packages, []lockfile.PackageDetails{
+		{
+			Name:      "certifi",
+			Version:   "2025.1.31",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "chardet",
+			Version:   "3.0.4",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "charset-normalizer",
+			Version:   "2.0.12",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "colorama",
+			Version:   "0.3.9",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "idna",
+			Version:   "2.7",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "py",
+			Version:   "1.4.34",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "pytest",
+			Version:   "3.2.5",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "requests",
+			Version:   "2.27.1",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "setuptools",
+			Version:   "39.2.0",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+		{
+			Name:      "urllib3",
+			Version:   "1.26.20",
+			Ecosystem: lockfile.PylockEcosystem,
+			CompareAs: lockfile.PylockEcosystem,
+		},
+	})
+}
